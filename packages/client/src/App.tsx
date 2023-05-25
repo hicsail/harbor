84f89574284c32df76be82ed9807eb945dd738a6
chrischo <chrischo@bu.edu>
2023-05-25 13:24:50 -0400

    grab token from url and redirect

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -4,24 +4,28 @@ import { Layout } from '@layouts/layout';
 import { SettingsProvider } from '@context/settings.context';
 import { AuthProvider } from '@context/auth.context';
 import { GraphqlProvider } from '@graphql/graphql-provider';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { Home } from '@pages/home';
+import { Callback } from '@pages/callback';
+import { Paths } from '@constants/paths';
 
 export const App: FC = () => {
   return (
     <SettingsProvider>
       <AuthProvider>
-        <GraphqlProvider>
-          <ThemeProvider>
-            <Layout>
-              <></>
-            </Layout>
-          </ThemeProvider>
-        </GraphqlProvider>
+        <Router>
+          <GraphqlProvider>
+            <ThemeProvider>
+              <Layout>
+                <Routes>
+                  <Route path={Paths.HOME} element={<Home />} />
+                  <Route path={Paths.AUTH_CALLBACK} element={<Callback />} />
+                </Routes>
+              </Layout>
+            </ThemeProvider>
+          </GraphqlProvider>
+        </Router>
       </AuthProvider>
-      <ThemeProvider>
-        <Layout>
-          <></>
-        </Layout>
-      </ThemeProvider>
     </SettingsProvider>
   );
 };
