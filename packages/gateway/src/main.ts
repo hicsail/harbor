42723556dc297698c8421c4efec864a54dd3be75
Greg Frasco <frascog17@gmail.com>
2023-06-03 10:50:16 -0400

    fix: add cors

--- packages/gateway/src/main.ts
+++ packages/gateway/src/main.ts
@@ -2,7 +2,13 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, { cors: true });
+  const app = await NestFactory.create(AppModule, {
+    cors: {
+      origin: '*',
+      methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+      preflightContinue: true
+    }
+  });
   await app.listen(3000);
 }
 bootstrap();
