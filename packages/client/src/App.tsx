2ec23875800ef92497ba8cac6f62c46adbcb5a15
chrischo <chrischo@bu.edu>
2023-06-05 13:16:24 -0700

    add login and logout routing

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -16,6 +16,8 @@ import { Settings } from '@pages/settings';
 import { AuthMethods } from '@pages/auth-methods';
 import { Home } from '@pages/home';
 import { ProjectProvider } from '@context/project.context';
+import { Login } from '@pages/login';
+import { Logout } from '@pages/logout';
 
 export const App: FC = () => {
   return (
@@ -39,6 +41,8 @@ export const App: FC = () => {
                     </Route>
                   </Route>
                   <Route path={Paths.AUTH_CALLBACK} element={<Callback />} />
+                  <Route path={Paths.LOGIN} element={<Login />} />
+                  <Route path={Paths.LOGOUT} element={<Logout />} />
                 </Routes>
               </ThemeProvider>
             </ProjectProvider>
---
+++ packages/client/src/pages/logout.tsx
@@ -0,0 +1,29 @@
+import { Paths } from '@constants/paths';
+import { Box, CircularProgress, Stack } from '@mui/material';
+import { FC, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+
+export const Logout: FC = () => {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    localStorage.removeItem('token');
+    navigate(Paths.LOGIN, { replace: true });
+  }, []);
+
+  return (
+    <Stack
+      spacing={4}
+      sx={{
+        mt: 10,
+        height: '100%',
+        width: '100%',
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center'
+      }}
+    >
+      <CircularProgress size={64} />
+    </Stack>
+  );
+};
