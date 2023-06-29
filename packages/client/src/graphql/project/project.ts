2e75689172e5abef18245c0510d13534febd6832
Christopher Cho <58639920+chrischo360@users.noreply.github.com>
2023-06-29 17:28:53 +0900

    Projects list (#20)
    * add nginx.conf file and update docker
    
    * add 404, permission, error, maintenance page
    
    * fix settings and auth methods in graphql call
    
    * add projects list
    
    * add condition for image

--- packages/client/src/graphql/project/project.ts
+++ packages/client/src/graphql/project/project.ts
@@ -23,7 +23,16 @@ export type GetProjectQuery = {
     createdAt: any;
     updatedAt: any;
     deletedAt?: any | null;
+    settings: { __typename?: 'ProjectSettingsModel'; allowSignup: boolean; displayProjectName: boolean };
+    authMethods: { __typename?: 'ProjectAuthMethodsModel'; emailAuth: boolean; googleAuth: boolean };
   };
+};
+
+export type ListProjectsQueryVariables = Types.Exact<{ [key: string]: never }>;
+
+export type ListProjectsQuery = {
+  __typename?: 'Query';
+  listProjects: Array<{ __typename?: 'ProjectModel'; id: string; name: string; description?: string | null; logo?: string | null }>;
 };
 
 export const GetProjectDocument = gql`
@@ -39,6 +48,14 @@ export const GetProjectDocument = gql`
       createdAt
       updatedAt
       deletedAt
+      settings {
+        allowSignup
+        displayProjectName
+      }
+      authMethods {
+        emailAuth
+        googleAuth
+      }
     }
   }
 `;
@@ -70,3 +87,40 @@ export function useGetProjectLazyQuery(baseOptions?: Apollo.LazyQueryHookOptions
 export type GetProjectQueryHookResult = ReturnType<typeof useGetProjectQuery>;
 export type GetProjectLazyQueryHookResult = ReturnType<typeof useGetProjectLazyQuery>;
 export type GetProjectQueryResult = Apollo.QueryResult<GetProjectQuery, GetProjectQueryVariables>;
+export const ListProjectsDocument = gql`
+  query listProjects {
+    listProjects {
+      id
+      name
+      description
+      logo
+    }
+  }
+`;
+
+/**
+ * __useListProjectsQuery__
+ *
+ * To run a query within a React component, call `useListProjectsQuery` and pass it any options that fit your needs.
+ * When your component renders, `useListProjectsQuery` returns an object from Apollo Client that contains loading, error, and data properties
+ * you can use to render your UI.
+ *
+ * @param baseOptions options that will be passed into the query, supported options are listed on: https://www.apollographql.com/docs/react/api/react-hooks/#options;
+ *
+ * @example
+ * const { data, loading, error } = useListProjectsQuery({
+ *   variables: {
+ *   },
+ * });
+ */
+export function useListProjectsQuery(baseOptions?: Apollo.QueryHookOptions<ListProjectsQuery, ListProjectsQueryVariables>) {
+  const options = { ...defaultOptions, ...baseOptions };
+  return Apollo.useQuery<ListProjectsQuery, ListProjectsQueryVariables>(ListProjectsDocument, options);
+}
+export function useListProjectsLazyQuery(baseOptions?: Apollo.LazyQueryHookOptions<ListProjectsQuery, ListProjectsQueryVariables>) {
+  const options = { ...defaultOptions, ...baseOptions };
+  return Apollo.useLazyQuery<ListProjectsQuery, ListProjectsQueryVariables>(ListProjectsDocument, options);
+}
+export type ListProjectsQueryHookResult = ReturnType<typeof useListProjectsQuery>;
+export type ListProjectsLazyQueryHookResult = ReturnType<typeof useListProjectsLazyQuery>;
+export type ListProjectsQueryResult = Apollo.QueryResult<ListProjectsQuery, ListProjectsQueryVariables>;
---
+++ packages/client/src/pages/project-list.tsx
@@ -0,0 +1,38 @@
+import { FC } from 'react';
+import { Box, Button, Card, CardContent, CardMedia, Typography } from '@mui/material';
+import { useListProjectsQuery } from '@graphql/project/project';
+import { Paths } from '@constants/paths';
+
+export const ProjectList: FC = () => {
+  const { data } = useListProjectsQuery();
+
+  const handleLogin = (projectId: string) => {
+    const harborCallbackUrl = encodeURIComponent(window.location.origin + Paths.AUTH_CALLBACK);
+    const loginUrl = `${import.meta.env.VITE_AUTHSERVICE_URL}`;
+    const redirectUrl = `${loginUrl}?projectId=${projectId}&redirectUrl=${harborCallbackUrl}`;
+    window.location.replace(redirectUrl);
+  };
+
+  return (
+    <>
+      {data?.listProjects.map((project) => (
+        <Card sx={{ display: 'flex', my: 2 }} key={project.id}>
+          <Box sx={{ display: 'flex', flexDirection: 'column', my: 2 }}>
+            <CardContent sx={{ flex: '1 0 auto', my: 2 }}>
+              <Typography component="div" variant="h5">
+                {project.name}
+              </Typography>
+              <Typography variant="subtitle1" color="text.secondary" component="div">
+                {project.description}
+              </Typography>
+            </CardContent>
+            <Box sx={{ display: 'flex', alignItems: 'center', pl: 1, pb: 1 }}>
+              <Button onClick={() => handleLogin(project.id)}>Login</Button>
+            </Box>
+          </Box>
+          <CardMedia component="img" sx={{ width: 151 }} image={project.logo || ''} alt="Project Logo" />
+        </Card>
+      ))}
+    </>
+  );
+};
