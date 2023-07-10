8c38657bf295ccc00139677c91816c0570842831
chrischo <chrischo@bu.edu>
2023-07-10 19:50:14 +0900

    add toggle for project settings

--- packages/client/src/pages/settings.tsx
+++ packages/client/src/pages/settings.tsx
@@ -1,15 +1,56 @@
-import { Card, CardContent, CardHeader, Typography } from '@mui/material';
+import { Card, CardContent, FormControlLabel, FormGroup, FormLabel, Grid, Switch } from '@mui/material';
 import { useProject } from '@context/project.context';
+import { useGetProjectQuery, useUpdateProjectSettingsMutation } from '@graphql/project/project';
+import { InputMaybe } from '@graphql/graphql';
+
+type Params = {
+  id: string;
+  displayProjectName?: InputMaybe<boolean> | undefined;
+  allowSignup?: InputMaybe<boolean> | undefined;
+};
 
 export const Settings = () => {
   const { project } = useProject();
+  const { data: projectData } = useGetProjectQuery({ variables: { id: project?.id || '' }, skip: !project?.id });
+  const [updateProjectSettings] = useUpdateProjectSettingsMutation();
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const params: Params = { id: project?.id || '' };
+    switch (event?.target.name) {
+      case 'displayProjectName':
+        params.displayProjectName = event.target.checked;
+        break;
+      case 'allowSignup':
+        params.allowSignup = event.target.checked;
+        break;
+    }
+
+    try {
+      updateProjectSettings({ variables: params });
+    } catch (error) {
+      console.error(error);
+      window.alert('Error in updating project');
+    }
+  };
 
   return (
     <Card>
-      <CardHeader title="Setting Details" />
       <CardContent>
-        <Typography>allowSignup: {project?.settings.allowSignup.toString()}</Typography>
-        <Typography>displayProjectName: {project?.settings.displayProjectName.toString()}</Typography>
+        <Grid container spacing={2}>
+          <Grid item xs={6}>
+            <FormLabel component="legend">Additional Settings</FormLabel>
+            <FormGroup>
+              <FormControlLabel
+                control={<Switch checked={projectData?.getProject.settings.displayProjectName || false} onChange={handleChange} name="displayProjectName" />}
+                label="displayProjectName"
+              />
+              <FormControlLabel
+                control={<Switch checked={projectData?.getProject.settings.allowSignup || false} onChange={handleChange} name="allowSignup" />}
+                label="allowSignup"
+              />
+            </FormGroup>
+          </Grid>
+        </Grid>
       </CardContent>
     </Card>
   );
