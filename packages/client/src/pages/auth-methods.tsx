0fe6b626c7724d957563daf496516a53e95253c7
Christopher Cho <58639920+chrischo360@users.noreply.github.com>
2023-07-12 19:43:54 +0900

    Update projectsettings (#23)
    * add nginx.conf file and update docker
    
    * add 404, permission, error, maintenance page
    
    * fix settings and auth methods in graphql call
    
    * add projects list
    
    * add condition for image
    
    * update users and invite list
    
    * remove useEffect
    
    * expired token
    
    * add toggle for project settings
    
    * remove unused loading and error

--- packages/client/src/pages/auth-methods.tsx
+++ packages/client/src/pages/auth-methods.tsx
@@ -1,15 +1,52 @@
 import { useProject } from '@context/project.context';
-import { Card, CardContent, CardHeader, Typography } from '@mui/material';
+import { InputMaybe } from '@graphql/graphql';
+import { useGetProjectQuery, useUpdateProjectAuthMethodsMutation } from '@graphql/project/project';
+import { Card, CardContent, FormControlLabel, FormGroup, FormLabel, Grid, Switch } from '@mui/material';
+
+type Params = {
+  id: string;
+  googleAuth?: InputMaybe<boolean> | undefined;
+  emailAuth?: InputMaybe<boolean> | undefined;
+};
 
 export const AuthMethods = () => {
   const { project } = useProject();
+  const { data: projectData } = useGetProjectQuery({ variables: { id: project?.id || '' }, skip: !project?.id });
+  const [updateProjectAuthMethods] = useUpdateProjectAuthMethodsMutation();
 
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const params: Params = { id: project?.id || '' };
+    switch (event?.target.name) {
+      case 'googleAuth':
+        params.googleAuth = event.target.checked;
+        break;
+      case 'emailAuth':
+        params.emailAuth = event.target.checked;
+        break;
+    }
+
+    try {
+      updateProjectAuthMethods({ variables: params });
+    } catch (error) {
+      console.error(error);
+      window.alert('Error in updating project');
+    }
+  };
   return (
     <Card>
-      <CardHeader title="Auth Methods Details" />
       <CardContent>
-        <Typography>emailAuth: {project?.authMethods.emailAuth.toString()}</Typography>
-        <Typography>googleAuth: {project?.authMethods.googleAuth.toString()}</Typography>
+        <Grid container spacing={2}>
+          <Grid item xs={6}>
+            <FormLabel component="legend">Auth Methods</FormLabel>
+            <FormGroup>
+              <FormControlLabel
+                control={<Switch checked={projectData?.getProject.authMethods.googleAuth || false} onChange={handleChange} name="googleAuth" />}
+                label="googleAuth"
+              />
+              <FormControlLabel control={<Switch checked={projectData?.getProject.authMethods.emailAuth || false} onChange={handleChange} name="emailAuth" />} label="emailAuth" />
+            </FormGroup>
+          </Grid>
+        </Grid>
       </CardContent>
     </Card>
   );
