520ba2273775f4ff256d01694b017487b3cd85e3
chrischo <chrischo@bu.edu>
2023-07-15 16:04:27 +0900

    remove unused variables

--- packages/client/src/pages/user.tsx
+++ packages/client/src/pages/user.tsx
@@ -1,17 +1,11 @@
-import { useProject } from '@context/project.context';
 import { Card, CardContent, CardHeader, Typography } from '@mui/material';
 import { useParams } from 'react-router-dom';
 import { useGetUserQuery } from '@graphql/user/user';
 
 export const User = () => {
-  const { project } = useProject();
   let { id } = useParams();
   const userId = id || '';
-  const {
-    data: userData,
-    loading,
-    error
-  } = useGetUserQuery({
+  const { data: userData } = useGetUserQuery({
     variables: {
       id: userId
     }
