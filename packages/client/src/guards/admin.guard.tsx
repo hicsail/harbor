60d83f662c358dcb2accee6164fa055ce210daf3
chrischo <chrischo@bu.edu>
2023-07-20 13:17:55 +0900

    add useEffect

--- packages/client/src/guards/admin.guard.tsx
+++ packages/client/src/guards/admin.guard.tsx
@@ -1,4 +1,4 @@
-import { FC } from 'react';
+import { FC, useEffect } from 'react';
 import { useAuth } from '@context/auth.context';
 import { Paths } from '@constants/paths';
 import { Outlet, useNavigate } from 'react-router-dom';
@@ -11,14 +11,15 @@ export const AdminGuard: FC = () => {
   const projectId = project?.id;
   const loginUrl = `${import.meta.env.VITE_AUTH_CLIENT}?projectId=${projectId}&redirectUrl=${encodeURIComponent(window.location.origin + Paths.AUTH_CALLBACK)}`;
 
-  if (initialized && !token) {
-    window.location.replace(loginUrl);
-    return null;
-  }
+  useEffect(() => {
+    if (initialized && !token) {
+      window.location.replace(loginUrl);
+    }
 
-  if (initialized && decoded_token?.role !== 1) {
-    navigate(Paths.PERMISSION_REQUIRED);
-  }
+    if (initialized && decoded_token?.role !== 1) {
+      navigate(Paths.PERMISSION_REQUIRED);
+    }
+  }, [initialized, token, decoded_token]);
 
   return <Outlet />;
 };
