cec2a2037cdc536b503381a11f38eda5f25a0749
chrischo <chrischo@bu.edu>
2023-07-20 13:31:32 +0900

    change useEffect

--- packages/client/src/guards/admin.guard.tsx
+++ packages/client/src/guards/admin.guard.tsx
@@ -8,18 +8,16 @@ export const AdminGuard: FC = () => {
   const { token, decoded_token, initialized } = useAuth();
   const { project } = useProject();
   const navigate = useNavigate();
-  const projectId = project?.id;
-  const loginUrl = `${import.meta.env.VITE_AUTH_CLIENT}?projectId=${projectId}&redirectUrl=${encodeURIComponent(window.location.origin + Paths.AUTH_CALLBACK)}`;
 
   useEffect(() => {
     if (initialized && !token) {
+      const projectId = project?.id;
+      const loginUrl = `${import.meta.env.VITE_AUTH_CLIENT}?projectId=${projectId}&redirectUrl=${encodeURIComponent(window.location.origin + Paths.AUTH_CALLBACK)}`;
       window.location.replace(loginUrl);
-    }
-
-    if (initialized && decoded_token?.role !== 1) {
+    } else if (initialized && decoded_token?.role !== 1) {
       navigate(Paths.PERMISSION_REQUIRED);
     }
-  }, [initialized, token, decoded_token]);
+  }, [initialized, token, decoded_token, project]);
 
   return <Outlet />;
 };
