cedc633c53d9e15ae1084cbdf67272cdf4d240df
Greg Frasco <frascog17@gmail.com>
2023-07-24 18:29:05 -0400

    fix: trying this file

--- packages/client/generate_env.sh
+++ packages/client/generate_env.sh
@@ -1,33 +1,43 @@
 #!/bin/bash
 
 # Recreate config file
-rm -rf ./env-config.js
+rm -f ./env-config.js
 touch ./env-config.js
 
-# Add assignment 
+# Add assignment
 echo "window._env_ = {" >> ./env-config.js
 
 # Read each line in .env file
 # Each line represents key=value pairs
-while read -r line || [[ -n "$line" ]];
+while IFS= read -r line || [[ -n "$line" ]];
 do
+  # Skip empty lines and comments
+  if [[ -z "$line" || "$line" =~ ^[[:space:]]*# ]]; then
+    continue
+  fi
+
   # Split env variables by character `=`
-  if printf '%s\n' "$line" | grep -q -e '='; then
-    varname=$(printf '%s\n' "$line" | sed -e 's/=.*//')
-    varvalue=$(printf '%s\n' "$line" | sed -e 's/^[^=]*=//')
+  if [[ "$line" == *=* ]]; then
+    varname="${line%%=*}"
+    varvalue="${line#*=}"
+  else
+    # Handle lines without an equal sign (invalid format)
+    echo "Invalid line in .env file: $line"
+    continue
   fi
 
   # Read value of current variable if exists as Environment variable
-  value=$(printf '%s\n' "${!varname}")
+  value="${!varname}"
+
   # Otherwise use value from .env file
-  [[ -z $value ]] && value=${varvalue}
+  [[ -z "$value" ]] && value="$varvalue"
 
   # Append configuration property to JS file if the key starts with VITE_
-  if [[ "$varname" == VITE_* ]]; then
+  if [[ "${varname:0:5}" == "VITE_" ]]; then
     echo "  $varname: \"$value\"," >> ./env-config.js
   fi
 
-  # echo "  $varname: \"$value\"," >> ./env-config.js
 done < .env
 
 echo "}" >> ./env-config.js
+echo "Done generating env.json file"
