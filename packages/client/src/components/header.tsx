7a4a0fdeb1371277ea73f43c32c03d64a4eb85db
chrischo <chrischo@bu.edu>
2023-08-01 14:50:38 +0900

    add custom theme and allow project settings to be changed

--- packages/client/src/components/header.tsx
+++ packages/client/src/components/header.tsx
@@ -2,9 +2,11 @@ import { FC } from 'react';
 import { useSettings } from '@context/settings.context';
 import { DarkMode, LightMode } from '@mui/icons-material';
 import { AppBar, IconButton, Toolbar, Typography } from '@mui/material';
+import { useProject } from '@context/project.context';
 
 export const Header: FC = () => {
   const { settings, setSettings } = useSettings();
+  const { project } = useProject();
 
   const toggleDarkMode = () => {
     setSettings({ ...settings, theme: settings.theme === 'light' ? 'dark' : 'light' });
@@ -14,7 +16,7 @@ export const Header: FC = () => {
     <AppBar position="fixed" elevation={0} sx={{ zIndex: (theme) => theme.zIndex.drawer + 1 }}>
       <Toolbar>
         <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-          Harbor
+          {project?.name}
         </Typography>
         <IconButton edge="start" color="inherit" aria-label="menu" onClick={toggleDarkMode}>
           {settings.theme === 'light' && <DarkMode color="info" />}
